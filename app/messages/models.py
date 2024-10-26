6563477831f2d737a0926a0fa857402e74bc5eeb
kirill3005 <kirill20093005@gmail.com>
2024-10-26 19:44:04 +0300

    tbank

--- app/messages/models.py
+++ app/messages/models.py
@@ -5,7 +5,7 @@ from sqlalchemy import ForeignKey, text, Text
 class Message(Base):
     id: Mapped[int_pk]
     conversation_id: Mapped[int]
-    user_token: Mapped[int]
+    user_token: Mapped[str]
     message: Mapped[str]
     photo: Mapped[str]
     sender: Mapped[str]
