18dc8cd20b4bf76be51f213a1eb776124c0e770b
kirill3005 <kirill20093005@gmail.com>
2024-10-31 11:23:24 +0300

    tbank

--- app/api/main.py
+++ app/api/main.py
@@ -7,6 +7,9 @@ from messages.dao import MessagesDAO, ConversationsDAO
 from messages.schemas import NewMessage
 
 from messages.models import Conversation
+from typing import Optional
+
+from app.databases.dao import DatabasesDAO
 
 app = FastAPI()
 
@@ -22,27 +25,46 @@ async def main_api(token: str):
     return user
 
 
-@app.post('/new_conversation/{token}/{project_token}',tags=['Создать новый диалог'])
-async def new_conv(token: str, project_token: str):
-    await ConversationsDAO.add(**{'user_token': token, 'project_token': project_token})
-    conv = (await ConversationsDAO.find_all(user_token=token, project_token=project_token))[-1]
-    return conv.id
+@app.post('/new_conversation',tags=['Создать новый диалог'])
+async def new_conv(api_token: Optional[str] = None, db_token: Optional[str] = None):
+    if api_token is None or db_token is None:
+        return {'message':'Неверный токен пользователя или баз данных', 'conv_id':None}
+    user = UsersDAO.find_one_or_none(token=api_token)
+    if not user:
+        return {'message':'Неверный токен пользователя', 'conv_id': None}
+    db = DatabasesDAO.find_one_or_none(user_token=api_token, token=db_token)
+    if not db:
+        return {'message':'Неверный токен базы данных', 'conv_id': None}
+    await ConversationsDAO.add(**{'user_token': api_token, 'project_token': db_token})
+    conv = (await ConversationsDAO.find_all(user_token=api_token, project_token=db_token))[-1]
+    return {'message':'OK', 'conv_id':conv.id}
 
 
-@app.post('/message/{token}/{project_token}/{conversation_id}',tags=['Передача сообщения от пользователя модели и получение ответа'])
-async def send_message(token: str, project_token:str, conversation_id: int, message: NewMessage):
-    user = await UsersDAO.find_one_or_none(token=token)
+@app.post('/message/',tags=['Передача сообщения от пользователя модели и получение ответа'])
+async def send_message(message: NewMessage, api_token: Optional[str] = None, db_token: Optional[str] = None, conversation_id: Optional[int]=None):
+    if api_token is None or db_token is None:
+        return {'message':'Неверный токен пользователя или баз данных', 'response': None}
+    user = UsersDAO.find_one_or_none(token=api_token)
+    if not user:
+        return {'message':'Неверный токен пользователя', 'response': None}
+    db = DatabasesDAO.find_one_or_none(user_token=api_token, token=db_token)
+    if not db:
+        return {'message':'Неверный токен базы данных', 'response': None}
+    conv = ConversationsDAO.find_one_or_none(user_token=api_token, project_token=db_token, id=conversation_id)
+    if not conv:
+        return {'message':'Неверный id диалога', 'response': None}
+    user = await UsersDAO.find_one_or_none(token=api_token)
     if user.tokens_count <= 0:
-        return {'message': 'У вас закончились токены'}
-    await UsersDAO.update(filter_by={'token': token},tokens_count=user.tokens_count - 1)
+        return {'message': 'У вас закончились токены', 'response': None}
+    await UsersDAO.update(filter_by={'token': api_token},tokens_count=user.tokens_count - 1)
     msg_dict = message.dict()
-    msg_dict['user_token'] = token
+    msg_dict['user_token'] = api_token
     msg_dict['sender'] = 'user'
     msg_dict['conversation_id'] = conversation_id
-    msg_dict['project_token'] = project_token
+    msg_dict['project_token'] = db_token
     await MessagesDAO.add(**msg_dict)
     '''response = model(message.message, message.photo)'''
-    response_dict = {'message': 'response', 'user_token': token, 'photo': '', 'sender': 'model',
-                     'conversation_id': conversation_id, 'project_token': project_token}
+    response_dict = {'message': 'response', 'user_token': api_token, 'photo': '', 'sender': 'model',
+                     'conversation_id': conversation_id, 'project_token': db_token}
     await MessagesDAO.add(**response_dict)
-    return 'Ответ от модели'
+    return {'message':'OK', 'response':'Ответ от модели'}
